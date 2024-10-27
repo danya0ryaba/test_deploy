b33b7746a0db8346a1a2805f9f82c6c0219cf3ae
Daniil Ryabenkov <danilryaba0@gmail.com>
2024-10-27 13:42:21 +0200

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 // https://www.youtube.com/watch?app=desktop&v=PN4uPBEfxCQ
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
+  <HashRouter basename="/test_deploy">
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
